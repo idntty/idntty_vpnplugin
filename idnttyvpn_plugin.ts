c0a94bd0fc75489d226de0bbd201ae49e6cb9656
idntty <hello@idntty.org>
2022-10-26 00:48:47 +0600

    Implement internode communication

--- idnttyvpn_plugin.ts
+++ idnttyvpn_plugin.ts
@@ -1,9 +1,9 @@
 const pluginPackage = require('./package.json');
-
 const { apiClient } = require('@liskhq/lisk-client');
-
 const { BasePlugin, BaseChannel, EventsDefinition, ActionsDefinition, SchemaWithDefault } = require("lisk-sdk");
+
 import { createClient } from 'redis';
+const sodium = require('sodium-native');
 
 const DB_KEY_FAUCET = 'VPN:';
 
@@ -25,35 +25,43 @@ export class IdnttyVPNPlugin extends BasePlugin {
 
 
     public get events(): EventsDefinition {
-      return ['tunnel'];
+      return ['peeradd', 'peerremove'];
     }
 
     public get actions(): ActionsDefinition {
         return {
-            servers: async ( ) => {              
+            servers: async ( ) => {
               return "pong:servers";
             },
-            getTunnels: async ( _user: object ) => {
-              return "pong:getTunnels";
-            },
-            createTunnels: async ( _auth: object ) => {
-              return "pong:createTunnels";
-            },
-            deleteTunnels: async ( _auth: object, tunnel: string) => {
-              return "pong:deleteTunnels";
+            peer: async ( _peer: object ) => {
+              console.log(_peer);
+              return "pong:peer";
             },
         };
     }
 
     public async load(channel: BaseChannel): Promise<void> {
 
-        this.db = createClient();
-        this.db.on('error', (err) => console.log('Redis Client Error', err));                    
-        await this.db.connect();
+      this._channel = channel;
 
-        this._channel.subscribe('app:block:new', (data) => {
-          this._channel.publish('IdnttyVPNPlugin:tunnel', { some : "value" });
-        });
+      this.db = createClient();
+      this.db.on('error', (err) => console.log('Redis Client Error', err));                    
+      await this.db.connect();
+
+      this._channel.subscribe('app:block:new', (data) => {
+
+        const decodedBlock = this.codec.decodeBlock(data.block);   
+
+        decodedBlock.payload.forEach(blockTransaction => {
+            blockTransaction.height = decodedBlock.header.height;
+            console.log(blockTransaction.moduleID, blockTransaction.assetID);
+            if (blockTransaction.moduleID == 1001 && blockTransaction.assetID == 1) { //some condition 
+              let x25519_pk = Buffer.alloc(sodium.crypto_box_PUBLICKEYBYTES);
+              sodium.crypto_sign_ed25519_pk_to_curve25519(x25519_pk, Buffer.from(blockTransaction.senderPublicKey, 'hex'));
+              this._channel.publish('idnttyvpn:addpeer', { publicKey : x25519_pk.toString('Base64') });
+            }
+        });        
+      });
 
     }
 
